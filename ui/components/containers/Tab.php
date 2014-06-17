d2fab93b4624154a99075ac715094c4c41c5d018
Erik Ashcraft <eriktashcraft@hotmail.com>
2014-06-17 00:56:56 -0500

    Create Tab.php

---
+++ ui/components/containers/Tab.php
@@ -0,0 +1,44 @@
+<?php 
+/*******************************************************************
+* Copyright (C) 2013 utilitron.net
+*
+* This software is provided 'as-is', without any express or implied
+* warranty. In no event will the authors be held liable for any damages
+* arising from the use of this software.
+
+* Permission is granted to anyone to use this software for any purpose,
+* including commercial applications, and to alter it and redistribute it
+* freely, without restriction.
+*
+* AUTHOR
+* ======
+* Erik Ashcraft
+*
+* Created: 2/12/2013
+*/
+
+if (!defined('ROOT_PATH')){
+	header('This is not the page you are looking for', true, 404);
+	include( dirname(dirname(dirname(dirname(dirname(__FILE__))))) . '/404.html');
+	exit();
+}
+
+require_once APP_PATH . "ui/components/Container.php";
+
+
+/**
+ * Tab Class
+ */
+Class Tab extends Container {
+
+	/**
+	 * Constructor
+	 */
+	public function __construct() {
+		parent::__construct();
+		
+		$this->element = 'li';
+
+	}
+}
+?>
