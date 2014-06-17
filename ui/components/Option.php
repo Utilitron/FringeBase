8595be04847d0281935577a09db563561bb9a210
Erik Ashcraft <eriktashcraft@hotmail.com>
2014-06-17 00:50:01 -0500

    Create Option.php

---
+++ ui/components/Option.php
@@ -0,0 +1,81 @@
+<?php 
+/*******************************************************************
+* Copyright (C) 2013 utilitron.net
+*
+* This software is provided 'as-is', without any express or implied
+* warranty. In no event will the authors be held liable for any damages
+* arising from the use of this software.
+
+* Permission is granted to anyone to use this software for any purpose,
+* including commercial applications, and to alter it and redistribute it
+* freely, without restriction.
+*
+* AUTHOR
+* ======
+* Erik Ashcraft
+*
+* Created: 2/9/2013
+*/
+
+if (!defined('ROOT_PATH')){
+	header('This is not the page you are looking for', true, 404);
+	include( dirname(dirname(dirname(dirname(dirname(__FILE__))))) . '/404.html');
+	exit();
+}
+
+require_once APP_PATH . "ui/Component.php";
+
+/**
+ * Option Class
+ */
+Class Option extends Component {
+
+	/**
+	* Text
+	* 
+	* @var text
+	*/
+	protected $text;
+	
+	/**
+	 * Value
+	 *
+	 * @var value
+	 */
+	protected $value;
+	
+	/**
+	 * Constructor
+	 */
+	public function __construct($value,$text=null) {
+		 parent::__construct();
+		 
+		 $this->element = 'option';
+		 
+		 $this->value = $value;
+		 $this->text = $text;
+	}
+	
+	/**
+	 * Build
+	 */
+	public function build($count_indent=0) {
+		$count_indent++;
+		
+		for ($i = 0; $i < $count_indent; $i++) echo "\t";
+		
+		echo '<'. $this->element;
+		if (!is_null($this->id))
+			echo ' id="'. $this->id .'"';
+			echo ' value="'. $this->value .'"';
+		echo '>';
+
+		if (!is_null($this->text))
+			echo $this->text;
+		
+		
+		echo '</'. $this->element .'>'. "\r\n";
+	}
+	
+}
+?>
